4e79d3b029c7ed9d2432f17ca153cbf6e474143c
Juan Alberto <japcreg@gmail.com>
2018-08-25 20:15:31 +0200

    Updated problem statement for decoded message

--- Problems/How many ways to decode a message/problem.py
+++ Problems/How many ways to decode a message/problem.py
@@ -2,6 +2,22 @@
 """
 How many ways to decode a message?
 
+Given the following mapping:
+
+'a' -> 1
+'b' -> 2
+'c' -> 3
+...
+'z' -> 26
+
+We can convert a message like "ab" to "12".
+
+Given a coded message, how many ways are to convert the coded message into the original one?
+
+For example:
+
+Coded message: "12"
+Possible original messages: {"ab","l"} as ("a" -> 1 and b -> "2" ) and ("l" -> "12")
 
 
 """
